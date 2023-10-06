078cd2ca8d5a6ccab8cd4677d6b74d6a6ebfbce8
Anmol8611 <kumaranmol8611@gmail.com>
2023-10-06 12:27:22 +0530

    added background video to the app

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 import SetAvatar from "./pages/SetAvatar";
 import ChatContainer from "./components/ChatContainer";
+import bgvideo from './assets/bgvideo.mp4'
 
 const App = () => {
   return (
-    <div className='bg-slate-950 bg-opacity-70 backdrop-filter backdrop-blur'>
+    <div className='bg-slate-950 bg-opacity-70 backdrop-filter backdrop-blur-lg'>
+      <div id='video-container'>
+        <video className="absolute h-screen w-screen object-cover object-center -z-20 opacity-20 bg-opacity-50 backdrop-filter backdrop-blur-md" src={bgvideo} muted autoPlay loop />
+      </div>
       <Routes>
         <Route path='/' element={<Chat />} />
-        <Route path="/currentChat" element={<ChatContainer/>} />
+        <Route path='/currentChat' element={<ChatContainer />} />
         <Route path='/register' element={<Register />} />
         <Route path='/login' element={<Login />} />
         <Route path='/setAvatar' element={<SetAvatar />} />
